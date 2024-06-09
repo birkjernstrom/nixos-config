fb36dd05865f1d489cf8a4f00b00e0734d902706
Birk Jernström <birkjernstrom@gmail.com>
2024-06-09 23:20:31 +0200

    macOS configuration

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,34 +1,92 @@
 { config, pkgs, ... }:
 
+let
+  user = "birk";
+in
 {
-    # List packages installed in system profile. To search by name, run:
-    # $ nix-env -qaP | grep wget
-    environment.systemPackages = [
-        pkgs.neovim
-    ];
+  imports = [
+      ./home.nix
+  ];
 
-    # Auto upgrade nix package and the daemon service.
-    services.nix-daemon.enable = true;
-    # nix.package = pkgs.nix;
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = [];
 
-    # Necessary for using flakes on this system.
-    nix.settings.experimental-features = "nix-command flakes";
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
 
-    # Create /etc/zshrc that loads the nix-darwin environment.
-    programs.zsh.enable = true;  # default shell on catalina
-    # programs.fish.enable = true;
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;  # default shell on catalina
+  # programs.fish.enable = true;
 
-    # Set Git commit hash for darwin-version.
-    system.configurationRevision = config.rev or config.dirtyRev or null;
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
 
+  # Setup user, packages, programs
+  nix = {
+    package = pkgs.nix;
+    settings.trusted-users = [ "@admin" "${user}" ];
+    settings.experimental-features = "nix-command flakes";
+
+    gc = {
+      user = "root";
+      automatic = true;
+      interval = { Weekday = 0; Hour = 2; Minute = 0; };
+      options = "--delete-older-than 30d";
+    };
+  };
+
+  fonts.fontDir.enable = true;
+
+  system = {
     # Used for backwards compatibility, please read the changelog before changing.
     # $ darwin-rebuild changelog
-    system.stateVersion = 4;
+    stateVersion = 4;
 
-    # The platform the configuration will be used on.
-    nixpkgs.hostPlatform = "aarch64-darwin";
+    # Set Git commit hash for darwin-version.
+    configurationRevision = config.rev or config.dirtyRev or null;
 
-    imports = [
-        ./home.nix
-    ];
+    defaults = {
+      NSGlobalDomain = {
+        AppleShowAllExtensions = true;
+        ApplePressAndHoldEnabled = false;
+
+        # 120, 90, 60, 30, 12, 6, 2
+        KeyRepeat = 2;
+
+        # 120, 94, 68, 35, 25, 15
+        InitialKeyRepeat = 15;
+
+        "com.apple.mouse.tapBehavior" = 1;
+        "com.apple.sound.beep.volume" = 0.0;
+        "com.apple.sound.beep.feedback" = 0;
+      };
+
+      dock = {
+        autohide = true;
+        show-recents = false;
+        launchanim = true;
+        mouse-over-hilite-stack = true;
+        orientation = "bottom";
+        tilesize = 48;
+        # Spaces, no re-arrange
+        mru-spaces = false;
+      };
+
+      finder = {
+        CreateDesktop = false;
+      };
+
+      trackpad = {
+        Clicking = true;
+        TrackpadThreeFingerDrag = true;
+      };
+    };
+
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToControl = true;
+    };
+  };
 }
