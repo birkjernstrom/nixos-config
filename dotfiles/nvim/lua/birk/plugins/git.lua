0e0e7144f7c4438f7d657b2f83bbca3c5ea49fca
Birk Jernstrom <birkjernstrom@gmail.com>
2024-07-11 21:40:24 +0200

    nvim: fugitive vs. neogit

--- dotfiles/nvim/lua/birk/plugins/git.lua
+++ dotfiles/nvim/lua/birk/plugins/git.lua
@@ -1,85 +1,79 @@
 return {
-  -- Git related plugins
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "sindrets/diffview.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-    keys = {
-      { "<leader>gs", ":Neogit<CR>", desc = "Git status" },
-      { "<leader>gp", ":Neogit push<CR>", desc = "Git push" },
-    },
-    config = true,
-  },
+	-- Git related plugins
+	{
+		"tpope/vim-fugitive",
+		keys = {
+			{ "<leader>gs", ":Git<CR>", desc = "Git status" },
+			{ "<leader>gp", ":Git push<CR>", desc = "Git push" },
+		},
+	},
 
-  "tpope/vim-rhubarb",
+	"tpope/vim-rhubarb",
 
-  {
-    -- Adds git related signs to the gutter, as well as utilities for managing changes
-    "lewis6991/gitsigns.nvim",
-    opts = {
-      -- See `:help gitsigns.txt`
-      signs = {
-        change = { text = "~" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
-      },
-      on_attach = function(bufnr)
-        -- don't override the built-in and fugitive keymaps
-        local gs = package.loaded.gitsigns
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
+	{
+		-- Adds git related signs to the gutter, as well as utilities for managing changes
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			-- See `:help gitsigns.txt`
+			signs = {
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+			on_attach = function(bufnr)
+				-- don't override the built-in and fugitive keymaps
+				local gs = package.loaded.gitsigns
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
 
-        -- Navigation
-        map("n", "]c", function()
-          if vim.wo.diff then
-            return "]c"
-          end
-          vim.schedule(function()
-            gs.next_hunk()
-          end)
-          return "<Ignore>"
-        end, { expr = true, desc = "Next hunk" })
+				-- Navigation
+				map("n", "]c", function()
+					if vim.wo.diff then
+						return "]c"
+					end
+					vim.schedule(function()
+						gs.next_hunk()
+					end)
+					return "<Ignore>"
+				end, { expr = true, desc = "Next hunk" })
 
-        map("n", "[c", function()
-          if vim.wo.diff then
-            return "[c"
-          end
-          vim.schedule(function()
-            gs.prev_hunk()
-          end)
-          return "<Ignore>"
-        end, { expr = true, desc = "Previous hunk" })
+				map("n", "[c", function()
+					if vim.wo.diff then
+						return "[c"
+					end
+					vim.schedule(function()
+						gs.prev_hunk()
+					end)
+					return "<Ignore>"
+				end, { expr = true, desc = "Previous hunk" })
 
-        -- Actions
-        map("n", "<leader>hs", gs.stage_hunk, { desc = "Stage hunk" })
-        map("n", "<leader>hr", gs.reset_hunk, { desc = "Reset hunk" })
-        map("v", "<leader>hs", function()
-          gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-        end, { desc = "Stage selected hunk" })
-        map("v", "<leader>hr", function()
-          gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-        end, { desc = "Reset selected hunk" })
-        map("n", "<leader>hS", gs.stage_buffer, { desc = "Stage buffer" })
-        map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "Undo stage hunk" })
-        map("n", "<leader>hR", gs.reset_buffer, { desc = "Reset buffer" })
-        map("n", "<leader>hp", gs.preview_hunk, { desc = "Preview hunk" })
-        map("n", "<leader>hb", function()
-          gs.blame_line({ full = true })
-        end, { desc = "Blame line" })
-        map("n", "<leader>tb", gs.toggle_current_line_blame, { desc = "Toggle blame" })
-        map("n", "<leader>hd", gs.diffthis, { desc = "Diff this" })
-        map("n", "<leader>hD", function()
-          gs.diffthis("~")
-        end, { desc = "Diff this (against HEAD~)" })
-        map("n", "<leader>td", gs.toggle_deleted, { desc = "Toggle deleted" })
-      end,
-    },
-  },
+				-- Actions
+				map("n", "<leader>hs", gs.stage_hunk, { desc = "Stage hunk" })
+				map("n", "<leader>hr", gs.reset_hunk, { desc = "Reset hunk" })
+				map("v", "<leader>hs", function()
+					gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, { desc = "Stage selected hunk" })
+				map("v", "<leader>hr", function()
+					gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, { desc = "Reset selected hunk" })
+				map("n", "<leader>hS", gs.stage_buffer, { desc = "Stage buffer" })
+				map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "Undo stage hunk" })
+				map("n", "<leader>hR", gs.reset_buffer, { desc = "Reset buffer" })
+				map("n", "<leader>hp", gs.preview_hunk, { desc = "Preview hunk" })
+				map("n", "<leader>hb", function()
+					gs.blame_line({ full = true })
+				end, { desc = "Blame line" })
+				map("n", "<leader>tb", gs.toggle_current_line_blame, { desc = "Toggle blame" })
+				map("n", "<leader>hd", gs.diffthis, { desc = "Diff this" })
+				map("n", "<leader>hD", function()
+					gs.diffthis("~")
+				end, { desc = "Diff this (against HEAD~)" })
+				map("n", "<leader>td", gs.toggle_deleted, { desc = "Toggle deleted" })
+			end,
+		},
+	},
 }
