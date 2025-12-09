3b246fd4ed6486c1d6f3395d4fbca4a81eec7de3
Birk Jernstrom <birkjernstrom@gmail.com>
2025-12-09 15:52:56 +0100

    fix(nvf): Incorrect textobjects configuration

--- home/modules/nvim/default.nix
+++ home/modules/nvim/default.nix
@@ -117,45 +117,47 @@
           autotagHtml = true;
           textobjects = {
             enable = true;
-            select = {
-              enable = true;
-              lookahead = true;
-              keymaps = {
-                "aa" = "@parameter.outer";
-                "ia" = "@parameter.inner";
-                "af" = "@function.outer";
-                "if" = "@function.inner";
-                "ac" = "@class.outer";
-                "ic" = "@class.inner";
+            setupOpts = {
+              select = {
+                enable = true;
+                lookahead = true;
+                keymaps = {
+                  "aa" = "@parameter.outer";
+                  "ia" = "@parameter.inner";
+                  "af" = "@function.outer";
+                  "if" = "@function.inner";
+                  "ac" = "@class.outer";
+                  "ic" = "@class.inner";
+                };
               };
-            };
-            move = {
-              enable = true;
-              set_jumps = true;
-              goto_next_start = {
-                "]m" = "@function.outer";
-                "]]" = "@class.outer";
+              move = {
+                enable = true;
+                set_jumps = true;
+                goto_next_start = {
+                  "]m" = "@function.outer";
+                  "]]" = "@class.outer";
+                };
+                goto_next_end = {
+                  "]M" = "@function.outer";
+                  "][" = "@class.outer";
+                };
+                goto_previous_start = {
+                  "[m" = "@function.outer";
+                  "[[" = "@class.outer";
+                };
+                goto_previous_end = {
+                  "[M" = "@function.outer";
+                  "[]" = "@class.outer";
+                };
               };
-              goto_next_end = {
-                "]M" = "@function.outer";
-                "][" = "@class.outer";
-              };
-              goto_previous_start = {
-                "[m" = "@function.outer";
-                "[[" = "@class.outer";
-              };
-              goto_previous_end = {
-                "[M" = "@function.outer";
-                "[]" = "@class.outer";
-              };
-            };
-            swap = {
-              enable = true;
-              swap_next = {
-                "<leader>a" = "@parameter.inner";
-              };
-              swap_previous = {
-                "<leader>A" = "@parameter.inner";
+              swap = {
+                enable = true;
+                swap_next = {
+                  "<leader>a" = "@parameter.inner";
+                };
+                swap_previous = {
+                  "<leader>A" = "@parameter.inner";
+                };
               };
             };
           };
