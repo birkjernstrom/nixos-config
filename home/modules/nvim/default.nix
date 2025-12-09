ce60996c7b5ce8d83c9ed9edbd0ee68f07b01c0e
Birk Jernstrom <birkjernstrom@gmail.com>
2025-12-09 15:42:11 +0100

    feat(nvf): Bunch of tweaks

--- home/modules/nvim/default.nix
+++ home/modules/nvim/default.nix
@@ -109,8 +109,56 @@
           rust.enable = true;
           go.enable = true;
           markdown.enable = true;
-          html.enable = true;
           tailwind.enable = true;
+          html.enable = true;
+        };
+
+        treesitter = {
+          autotagHtml = true;
+          textobjects = {
+            enable = true;
+            select = {
+              enable = true;
+              lookahead = true;
+              keymaps = {
+                "aa" = "@parameter.outer";
+                "ia" = "@parameter.inner";
+                "af" = "@function.outer";
+                "if" = "@function.inner";
+                "ac" = "@class.outer";
+                "ic" = "@class.inner";
+              };
+            };
+            move = {
+              enable = true;
+              set_jumps = true;
+              goto_next_start = {
+                "]m" = "@function.outer";
+                "]]" = "@class.outer";
+              };
+              goto_next_end = {
+                "]M" = "@function.outer";
+                "][" = "@class.outer";
+              };
+              goto_previous_start = {
+                "[m" = "@function.outer";
+                "[[" = "@class.outer";
+              };
+              goto_previous_end = {
+                "[M" = "@function.outer";
+                "[]" = "@class.outer";
+              };
+            };
+            swap = {
+              enable = true;
+              swap_next = {
+                "<leader>a" = "@parameter.inner";
+              };
+              swap_previous = {
+                "<leader>A" = "@parameter.inner";
+              };
+            };
+          };
         };
       };
     };
